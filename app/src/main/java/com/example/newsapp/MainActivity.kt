5c1662057d561d1cc368760955b2489bd5faa783
PawarAyush <pawarayush2003@gmail>
2024-03-30 00:32:49 +0530

    first commit

---
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -0,0 +1,101 @@
+package com.example.newsapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.newsapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var myRecyclerView: RecyclerView
+    lateinit var newsArrayList: ArrayList<News>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding =ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        myRecyclerView=binding.RecyclerView
+
+        val newsImageArray = arrayOf(
+            R.drawable.pic1,
+            R.drawable.pic2,
+            R.drawable.pic3,
+            R.drawable.pic4,
+            R.drawable.pic5,
+            R.drawable.pic6,
+            R.drawable.img1,
+            R.drawable.img2,
+            R.drawable.img3,
+            R.drawable.img4,
+            R.drawable.img5,
+            R.drawable.img6,
+
+
+
+        )
+        val newsHeadingArray = arrayOf(
+            "EAM Jaishankar, Ukrainian foreign minister Kuleba hold bilateral talks",
+            "Gagan Shakti-2024’: IAF to kick off mega 10-day drills on April 1",
+            "Rahul Gandhi's big Lok Sabha poll promise for women",
+            "PM Modi, Bill Gates discuss AI, digital divide and India’s leading role in tech",
+            "‘Kejriwal ko Ashirwad’: Wife Sunita announces WhatsApp campaign for jailed CM",
+            "Only PM Modi and Amit Shah can resolve issues at national level: Ex-PM Gowda",
+            "U.K. Foreign Secretary James Cleverly raises issue of BBC tax searches with EAM Jaishankar",
+            "Cooking gas prices hiked by ₹50 for domestic, ₹350.50 for commercial cylinders",
+            "Joe Biden appoints two prominent Indian-American corporate leaders to his Export Council",
+            "Sergey Lavrov will raise suspected bombing of Nord Stream II at G20: Russian Foreign Ministry",
+            "Belarusian leader Lukashenko visits China amid Ukraine tensions",
+            "China rips new U.S. House committee on countering Beijing",
+            "Belarusian leader Lukashenko visits China amid Ukraine tensions",
+            "Largest gathering of Foreign Ministers hosted by any G20 presidency: Foreign Secretary Vinay Kwatra"
+        )
+
+        val newsContent = arrayOf(
+            getString(R.string.news_content), getString(R.string.news_content),
+            getString(R.string.news_content), getString(R.string.news_content),
+            getString(R.string.news_content), getString(R.string.news_content),
+            getString(R.string.news_content), getString(R.string.news_content),
+            getString(R.string.news_content), getString(R.string.news_content),
+            getString(R.string.news_content), getString(R.string.news_content)
+        )
+
+        myRecyclerView.layoutManager = LinearLayoutManager(this)
+        newsArrayList = arrayListOf<News>()
+
+
+        for( index in newsImageArray.indices){
+            val news = News(newsHeadingArray[index], newsImageArray[index] , newsContent[index])
+            newsArrayList.add(news)
+        }
+        var myAdapter = MyAdapter(newsArrayList, this)
+        myRecyclerView.adapter = myAdapter
+
+        myAdapter.setItemClickListener(object : MyAdapter.onItemClickListener {
+            override fun onItemClicking(position: Int) {
+                // on clicking each item , what action do you want to perform
+
+                val intent = Intent(this@MainActivity, NewsDetailActivity::class.java)
+                intent.putExtra("heading", newsArrayList[position].newsHeading)
+                intent.putExtra("imageId", newsArrayList[position].newsImage)
+                intent.putExtra("newscontent", newsArrayList[position].newsContent)
+                startActivity(intent)
+            }
+
+        })
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/newsapp/NewsDetailActivity.kt
@@ -0,0 +1,36 @@
+package com.example.newsapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.newsapp.databinding.ActivityMainBinding
+import com.example.newsapp.databinding.ActivityNewsDetailBinding
+
+class NewsDetailActivity : AppCompatActivity() {
+    lateinit var binding: ActivityNewsDetailBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityNewsDetailBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val heading = intent.getStringExtra("heading")
+        val newsContent = intent.getStringExtra("newscontent")
+        val imageId = intent.getIntExtra("imageId", R.drawable.img1)
+
+        val headingTV = binding.newsHeading
+        val headingIV = binding.newsImage
+        val newsContentTV = binding.newsContent
+
+        headingTV.text = heading
+        newsContentTV.text = newsContent
+        headingIV.setImageResource(imageId)
+    }
+}
